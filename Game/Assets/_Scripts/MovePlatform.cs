91ae9aee41e0fc25312919db12e32682c7285f8e
DESKTOP-J2BSH1E\Marishka <marishka.zachariah@globacore.com>
2017-04-16 22:38:43 -0400

    Setting up scene.

---
+++ Game/Assets/_Scripts/MovePlatform.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlatform : MonoBehaviour {
+
+	public GameObject player;
+	private Vector3 offset; 
+
+	void Start ()
+    {
+    }
+
+    void Update ()
+    {
+
+	}
+
+	public void StartPlatform()
+    {
+        iTween.MoveTo(gameObject, iTween.Hash("path", iTweenPath.GetPath("IntroPlatform"), "time", 5, "easetype", iTween.EaseType.linear));
+    }
+
+    public void MoveIntoBody()
+    {
+        iTween.MoveTo(gameObject, iTween.Hash("path", iTweenPath.GetPath("IntoBody"), "time", 5, "easetype", iTween.EaseType.linear));
+    }
+}
---
+++ Game/Assets/_Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public GameObject platform;
+
+    private Vector3 offset;
+
+    private MovePlatform _movePlatform;
+
+	void Start () {
+		offset = new Vector3 (0, 1, 0);
+        _movePlatform = FindObjectOfType<MovePlatform>();
+	}
+
+	void Update () 
+	{
+	}
+
+	public void OnCollisionEnter (Collision collision)
+	{
+        if (collision.gameObject.tag == "tool")
+        {
+            transform.position = platform.transform.position + offset;
+            _movePlatform.StartPlatform();
+        }
+	}
+}
