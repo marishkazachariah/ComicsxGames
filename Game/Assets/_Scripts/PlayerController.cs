01742fd1389249dc55ca197d90f11e7755e027b8
DESKTOP-J2BSH1E\Marishka <marishka.zachariah@globacore.com>
2017-04-22 21:02:53 -0400

    moving raft

--- Game/Assets/_Scripts/PlayerController.cs
+++ Game/Assets/_Scripts/PlayerController.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour {
 
 	public GameObject platform;
-
+    public GameObject raft;
     private Vector3 offset;
 
     private MovePlatform _movePlatform;
@@ -21,6 +21,10 @@ public class PlayerController : MonoBehaviour {
         {
             MoveOnPlatform();
         }
+        if (Input.GetKeyDown(KeyCode.U))
+        {
+            MoveRaft();
+        }
     }
 
 	public void OnCollisionEnter (Collision collision)
@@ -30,11 +34,29 @@ public class PlayerController : MonoBehaviour {
             transform.position = platform.transform.position + offset;
             _movePlatform.StartPlatform();
         }
-	}
+        if (collision.gameObject.tag == "raft")
+        {
+            MoveRaft();
+            Debug.Log("moving");
+        }
+    }
+
+    //public void OnTriggerEnter(Collider collider)
+    //{
+    //    if (collider.gameObject.tag == "raft")
+    //    {
+    //        MoveRaft();
+    //    }
+    //}
 
     public void MoveOnPlatform()
     {
         transform.position = platform.transform.position + offset;
         _movePlatform.StartPlatform();
+    }
+
+    public void MoveRaft()
+    {
+        iTween.MoveTo(raft, iTween.Hash("path", iTweenPath.GetPath("RaftPath"), "time", 5, "easetype", iTween.EaseType.linear));
     }
 }
